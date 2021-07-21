17712556b96af84a9bb69ec7671150a94998966a
Dipak Italiya <61690064+dipakibl@users.noreply.github.com>
2021-07-21 18:24:42 +0530

    Add files via upload

---
+++ ControlableAccess/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ControlableAccess
+{
+    class Program
+    {
+        [Flags]
+        public enum Controlable
+        {
+            Balance = 1, // 2^0
+            thirthpartyCall = 2, // 2^1     
+            chargeCalculation = 4, // 2^2
+            LimiteCalculation = 8, // 2^3 
+            MCC = 16// 2^4  
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scheme 1 has access of modify? =>  " + IsControled(24, Controlable.thirthpartyCall));          
+            Console.WriteLine("Scheme 2 has access of modify? =>  " + IsControled(18, Controlable.LimiteCalculation));          
+            Console.WriteLine("Scheme 3 has access of chargeCalculation ? =>  " + IsControled(24, Controlable.chargeCalculation));          
+        }
+
+        public static Boolean IsControled(long accessCount, Enum flag)
+        {
+            return ((Convert.ToUInt64(accessCount) & Convert.ToUInt64(flag)) == Convert.ToUInt64(flag));
+        }
+    }
+}
